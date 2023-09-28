8af576b47e83d9b5d9fc24fcd9f30c8018c9822e
Alan <alanchen.2003@gmail.com>
2023-09-29 02:49:21 +1000

    Update bar length according to sensor value

--- static/js/sensors.js
+++ static/js/sensors.js
@@ -1,10 +1,19 @@
-function updateValue(id,value) {
-    document.getElementById(id).innerHTML = value;
+function updateValue(id, value) {
+    let el = document.getElementById(id);
+    el.innerHTML = value;
+    el = el.parentElement.getElementsByTagName("svg")[0].firstElementChild;
+    if (value < 0) {
+        el.setAttribute("width", -value);
+        el.setAttribute("x", value);
+    } else {
+        el.setAttribute("width", value);
+        el.setAttribute("x", 0);
+    }
 }
 const eventSource = new EventSource("sensordata");
 
-eventSource.addEventListener("textData", event => {
+eventSource.addEventListener("textData", function (event) {
     data = JSON.parse(event.data);
-    console.log(data);
-    updateValue(data.id,data.payload);
+    //console.log(data);
+    updateValue(data.id, data.payload);
 });
