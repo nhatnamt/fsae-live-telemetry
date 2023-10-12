b9b0f6cd160193150034ce14459a6dc1b9ee4ee6
Alan <alanchen.2003@gmail.com>
2023-10-13 01:13:21 +1100

    Generate new timestamps for replayed events

--- src/playback_event_generator.py
+++ src/playback_event_generator.py
@@ -11,21 +11,28 @@ PLAYBACK_FILE = "static/data/playback.csv"
 def playback_event_generator():
     with open(PLAYBACK_FILE, newline="") as f:
         rows = csv.DictReader(f) # default delimiters
-        row = next(rows)
-        event = Event(**row)
-        offset = time.perf_counter() - float(event.timestamp)
-        print(event.timestamp, 0)
+        events = generate_from_csv(rows)
+        event = next(events)
+        offset = time.perf_counter() - event.timestamp
+        #print(event.timestamp, 0)
+        event.timestamp = time.perf_counter()
         yield event
-        for row in rows:
-            event = Event(**row)
-            sleep_time = float(event.timestamp) + offset - time.perf_counter()
-            print(event.timestamp, sleep_time)
+        for event in events:
+            sleep_time = event.timestamp + offset - time.perf_counter()
             if sleep_time > 0:
                 time.sleep(sleep_time)
+            #print(event.timestamp, sleep_time * -1000, "ms")
+            event.timestamp = time.perf_counter()
             yield event
 
+def generate_from_csv(rows):
+    for row in rows:
+        event = Event(**row)
+        event.timestamp = float(event.timestamp)
+        yield event
+
 if __name__ == "__main__":
-    count = 1000
+    count = 5
     for event in playback_event_generator():
         print(event)
         if count == 0:
