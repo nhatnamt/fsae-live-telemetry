287cccce1f39c6498ac8cd3ce2adbe86638b6c3d
Alan <alanchen.2003@gmail.com>
2023-10-16 20:52:08 +1100

    Replay locally from CSV on web server

--- static/js/sensors.js
+++ static/js/sensors.js
@@ -1,16 +1,68 @@
+/** @type {HTMLSelectElement} */
+var select = document.getElementById("csv");
+var csv_url = new URLSearchParams(window.location.search).get("data");
+
+// Update select box to show available CSV files
+fetch("data").then(function (response) {
+    if (response.ok) {
+        response.json().then(function (files) {
+            files.forEach(function (f) {
+                select.add(new Option(f, f));
+            });
+
+            select.value = csv_url;
+        });
+    }
+});
+
+select.onchange = function (ev) {
+    //console.log(window.location.href);
+    let option = ev.target.value;
+    if (option) {
+        option = "?data=" + option;
+    }
+    window.location.href = "sensors.html" + option;
+}
+
 function updateValue(id, value) {
     let el = document.getElementById(id);
     el.innerHTML = value;
     el = el.parentElement.querySelector("svg rect");
     el.dataset.value = value;
 }
-const eventSource = new EventSource("sensordata");
 
-eventSource.addEventListener("textData", function (event) {
-    let data = JSON.parse(event.data);
-    //console.log(data);
-    updateValue(data.id, data.payload);
-});
+// Check if URL points to a CSV file
+var csv_data;
+if (csv_url === null) {
+    // Use sensor data from web server
+    const eventSource = new EventSource("sensordata");
+
+    eventSource.addEventListener("textData", function (event) {
+        let data = JSON.parse(event.data);
+        //console.log(data);
+        updateValue(data.id, data.payload);
+    });
+} else {
+    // Use CSV file
+    d3.csv("/data/" + csv_url).then(function (data) {
+        if (data.length == 0) {
+            csv_data = data;
+            return;
+        }
+
+        let offset = parseFloat(data[0].timestamp);
+        data.forEach(function (d) {
+            // convert seconds to milliseconds
+            d.timestamp = (parseFloat(d.timestamp) - offset) * 1000;
+        })
+        csv_data = data;
+        
+        // set timeouts to update page values
+        csv_data.forEach(function (d) {
+            setTimeout(updateValue, d.timestamp, d.id, d.payload);
+        });
+    });
+}
 
 function barAnimationStep() {
     document.querySelectorAll(".text-data svg rect").forEach(function (el) {
